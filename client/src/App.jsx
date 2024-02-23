8efd77b7993b5b128cbed2852c5f1bccf44a866f
jay gupta <jayesh.gupta3224@gmail.com>
2024-02-23 14:10:52 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+    <p className='bg-red-400'>fghg</p>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
